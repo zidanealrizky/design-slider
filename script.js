395324dd183b12d1a10fda0583e975d344b3728a
Muhammad Zidane Alrizky <zidanealrizky@gmail.com>
2024-01-07 23:45:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// carousel
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+let carouselDom = document.querySelector(".carousel");
+let listItemDom = document.querySelector(".carousel .list");
+let thumbnailDom = document.querySelector(".carousel .thumbnail");
+
+let timeRunning = 3000;
+let runTimeOut;
+
+// automatic slide
+let timeAutoNext = 7000;
+let runAutoRun = setTimeout(() => {
+  nextDom.click();
+}, timeAutoNext);
+
+// fungsi showslider
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll(".carousel .list .item");
+  let itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item");
+
+  if (type === "next") {
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add("next");
+  } else {
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add("prev");
+  }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, timeRunning);
+
+  clearTimeout(runAutoRun);
+  runAutoRun = setTimeout(() => {
+    nextDom.click();
+  }, timeAutoNext);
+}
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+
+prevDom.onclick = function () {
+  showSlider("prev");
+};
